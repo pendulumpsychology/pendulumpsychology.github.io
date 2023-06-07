36fb5c22520e577d2fa3cc95ef24a329ea25055b
David O'Donohue <virtuspsychology@gmail.com>
2023-06-07 12:40:12 +1000

    commit

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('toggle').addEventListener('click', function () {
-    const toggleButton = document.getElementById('toggle');
-    const navButtons = document.querySelectorAll('.nav-button');
-
-    if (toggleButton.textContent === 'Expand menu') {
-      for (const navButton of navButtons) {
-        navButton.style.display = 'flex';
-      }
-      toggleButton.textContent = 'Collapse menu';
-    } else {
-      for (const navButton of navButtons) {
-        navButton.style.display = 'none';
-      }
-      toggleButton.textContent = 'Expand menu';
-    }
-  });
-
-  function adjustMainOffset() {
-    const navbar = document.getElementById('navbar');
-    const main = document.querySelector('main');
-
-    if (navbar && main) {
-      const navbarHeight = navbar.offsetHeight;
-      main.style.marginTop = `${navbarHeight}px`;
-    } else {
-      console.warn('Error: navbar or main element not found');
-    }
+  function loadComponent(component, elementId) {
+    return fetch(component)
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById(elementId).innerHTML = html;
+      })
+      .catch((error) => {
+        console.warn('Error loading', component, error);
+      });
   }
 
-  adjustMainOffset();
-  window.addEventListener('resize', adjustMainOffset);
+  Promise.all([
+    loadComponent('/navbar.html', 'navbar-container'),
+    loadComponent('/footer.html', 'footer-container'),
+  ]).then(() => {
+    document.getElementById('toggle').addEventListener('click', function () {
+      const toggleButton = document.getElementById('toggle');
+      const navButtons = document.querySelectorAll('.nav-button');
+
+      if (toggleButton.textContent === 'Expand menu') {
+        for (const navButton of navButtons) {
+          navButton.style.display = 'flex';
+        }
+        toggleButton.textContent = 'Collapse menu';
+      } else {
+        for (const navButton of navButtons) {
+          navButton.style.display = 'none';
+        }
+        toggleButton.textContent = 'Expand menu';
+      }
+    });
+    function adjustMainOffset() {
+      const navbar = document.getElementById('navbar');
+      const main = document.querySelector('main');
+    
+      if (navbar && main) {
+        const navbarHeight = navbar.offsetHeight; 
+        main.style.marginTop = `${navbarHeight}px`;
+      } else {
+        console.warn('Error: navbar or main element not found');
+      }
+    }
+    
+    adjustMainOffset();
+    window.addEventListener('resize', adjustMainOffset);
+  });
 });
